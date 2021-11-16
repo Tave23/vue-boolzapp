f226991df3e9fd416bf8e5a9cccd306ae2d74916
Davide Tavella <tavella01@gmail.com>
2021-11-16 10:34:52 +0100

    new array

--- MILESTONE 2/js/script.js
+++ MILESTONE 2/js/script.js
@@ -7,111 +7,87 @@ const app = new Vue({
    el: '#app',
 
    data:{
-      contatti:[
+      contacts: [
          {
-            nome: 'Michele',
-            avatar:'_2',
-            visible: true,
-            messaggi:[
-               {
-                  dataMessaggio: '10/01/2020 15:30:55',
-                  text:'Che hai fatto di bello oggi?',
-                  statoMessaggio: 'sent'
-               },
-               {
-                  dataMessaggio: '10/01/2020 15:31:55',
-                  text:'Niente di che, tu?',
-                  statoMessaggio: 'received'
-               },
-               {
-                  dataMessaggio: '10/01/2020 15:32:15',
-                  text:'Ho studiato',
-                  statoMessaggio: 'sent'
-               },
-               {
-                  dataMessaggio: '10/01/2020 15:32:55',
-                  text:'Sono stanchissimo',
-                  statoMessaggio: 'sent'
-               },
-            ]
+             name: 'Michele',
+             avatar: '_1',
+             visible: true,
+             messages: [{
+                 date: '10/01/2020 15:30:55',
+                 message: 'Hai portato a spasso il cane?',
+                 status: 'sent'
+             },
+                 {
+                     date: '10/01/2020 15:50:00',
+                     message: 'Ricordati di dargli da mangiare',
+                     status: 'sent'
+                 },
+                 {
+                     date: '10/01/2020 16:15:22',
+                     message: 'Tutto fatto!',
+                     status: 'received'
+                 }
+             ],
          },
          {
-            nome: 'Andrea',
-            avatar:'_3',
-            visible: true,
-            messaggi:[
-               {
-                  dataMessaggio: '10/01/2020 15:30:55',
-                  text:'Come stai?',
-                  statoMessaggio: 'sent'
-               },
-               {
-                  dataMessaggio: '10/01/2020 15:31:55',
-                  text:'bene, tu?',
-                  statoMessaggio: 'received'
-               },
-               {
-                  dataMessaggio: '10/01/2020 15:32:15',
-                  text:'Bella Milano?',
-                  statoMessaggio: 'received'
-               },
-               {
-                  dataMessaggio: '10/01/2020 15:32:55',
-                  text:'Bellissima',
-                  statoMessaggio: 'sent'
-               },
-            ]
+             name: 'Fabio',
+             avatar: '_2',
+             visible: true,
+             messages: [{
+                 date: '20/03/2020 16:30:00',
+                 message: 'Ciao come stai?',
+                 status: 'sent'
+             },
+                 {
+                     date: '20/03/2020 16:30:55',
+                     message: 'Bene grazie! Stasera ci vediamo?',
+                     status: 'received'
+                 },
+                 {
+                     date: '20/03/2020 16:35:00',
+                     message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                     status: 'received'
+                 }
+             ],
          },
          {
-            nome: 'Giorgia',
-            avatar:'_4',
-            visible: true,
-            messaggi:[
-               {
-                  dataMessaggio: '10/01/2020 15:30:55',
-                  text:'Grande partita ieri',
-                  statoMessaggio: 'received'
-               },
-               {
-                  dataMessaggio: '10/01/2020 15:31:55',
-                  text:'E che gol!!!',
-                  statoMessaggio: 'received'
-               },
-               {
-                  dataMessaggio: '10/01/2020 15:32:15',
-                  text:'Si bello tutto',
-                  statoMessaggio: 'sent'
-               },
-               {
-                  dataMessaggio: '10/01/2020 15:32:55',
-                  text:"Ma com'è che abbiamo perso 5-1",
-                  statoMessaggio: 'sent'
-               },
-            ]
+             name: 'Samuele',
+             avatar: '_3',
+             visible: true,
+             messages: [{
+                 date: '28/03/2020 10:10:40',
+                 message: 'La Marianna va in campagna',
+                 status: 'received'
+             },
+                 {
+                     date: '28/03/2020 10:20:10',
+                     message: 'Sicuro di non aver sbagliato chat?',
+                     status: 'sent'
+                 },
+                 {
+                     date: '28/03/2020 16:15:22',
+                     message: 'Ah scusa!',
+                     status: 'received'
+                 }
+             ],
          },
          {
-            nome: 'Giacomo',
-            avatar:'_5',
-            visible: true,
-            messaggi:[
-               {
-                  dataMessaggio: '10/01/2020 15:30:55',
-                  text:"Non c'è l'agvzzzz",
-                  statoMessaggio: 'sent'
-               },
-               {
-                  dataMessaggio: '10/01/2020 15:31:55',
-                  text:"C'è la manetta!",
-                  statoMessaggio: 'sent'
-               },
-               {
-                  dataMessaggio: '10/01/2020 15:32:15',
-                  text:'Proprio la baracca super hai comprato!',
-                  statoMessaggio: 'received'
-               },
-            ]
+             name: 'Luisa',
+             avatar: '_4',
+             visible: true,
+             messages: [{
+                 date: '10/01/2020 15:30:55',
+                 message: 'Lo sai che ha aperto una nuova pizzeria?',
+                 status: 'sent'
+             },
+                 {
+                     date: '10/01/2020 15:50:00',
+                     message: 'Si, ma preferirei andare al cinema',
+                     status: 'received'
+                 }
+             ],
          },
-      ]
+     ]
    },
 
    mounted(){
